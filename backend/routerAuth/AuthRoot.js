622ec2ab0a028394dfef383da7ed76501979333e
salahhlel <salahhlel123@gmail.com>
2024-03-14 22:38:15 +0100

    test

--- backend/routerAuth/AuthRoot.js
+++ backend/routerAuth/AuthRoot.js
@@ -5,7 +5,8 @@ const login = require('../controlerAuth/authentification.js');
 const userController = require('../controlerAuth/userController.js');
 
 router.post('/login', login.loginUser);
-router.post('/register', userController.createUser);
+router.post('/registerMan', userController.createUser);
+router.post('/registerAnim', userController.createAnimateur)
 router.post('/pass', login.checkPass);
 
 module.exports = router;
