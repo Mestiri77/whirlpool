b0d2e741decd809b2286d0f61cc1d36cc44be4ba
azizchannoufi <channoufi2003@gmail.com>
2024-04-03 00:23:56 +0100

    mise a jour 2 front

--- front-end/whirlpool/App.js
+++ front-end/whirlpool/App.js
@@ -6,7 +6,6 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Login/>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,7 +15,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
 });
